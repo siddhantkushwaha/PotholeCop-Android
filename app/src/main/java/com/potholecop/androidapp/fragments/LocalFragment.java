6f6157bb94345cc4c12444b9841a4b90ead4a65c
Siddhant Kushwaha <>
2018-09-22 14:25:50 +0530

    maps added

--- app/src/main/java/com/potholecop/androidapp/fragments/LocalFragment.java
+++ app/src/main/java/com/potholecop/androidapp/fragments/LocalFragment.java
@@ -1,21 +1,37 @@
 package com.potholecop.androidapp.fragments;
 
-import android.support.v4.app.Fragment;
+import android.app.Activity;
+import android.content.Context;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
 import com.potholecop.androidapp.R;
 
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.Unbinder;
+
 public class LocalFragment extends Fragment {
+
+    Unbinder unbinder;
 
     public static LocalFragment newInstance() {
 
         LocalFragment fragment = new LocalFragment();
         return fragment;
     }
+
+    private FragmentActivity context;
+    private GoogleMap mMap;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -26,6 +42,25 @@ public class LocalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_local, container, false);
+        unbinder = ButterKnife.bind(this, view);
+
+        context = (FragmentActivity)view.getContext();
+
+        SupportMapFragment mapFragment = (SupportMapFragment)getChildFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                mMap = googleMap;
+            }
+        });
+
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        unbinder.unbind();
     }
 }
