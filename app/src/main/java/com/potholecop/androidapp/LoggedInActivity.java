9c1b03517478253ccb73acf3ffa8920b8c35e47b
Siddhant Kushwaha <>
2018-09-22 14:46:19 +0530

    crop + report pothole

--- app/src/main/java/com/potholecop/androidapp/LoggedInActivity.java
+++ app/src/main/java/com/potholecop/androidapp/LoggedInActivity.java
@@ -5,9 +5,12 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 import com.potholecop.androidapp.fragments.FeedFragment;
 import com.potholecop.androidapp.fragments.LocalFragment;
@@ -46,6 +49,12 @@ public class LoggedInActivity extends AppCompatActivity {
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
         navigation.setSelectedItemId(R.id.navigation_home);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        return true;
     }
 
     private void openFragment(Fragment fragment) {
